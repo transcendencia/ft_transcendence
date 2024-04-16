2373fe40c7fd69ffbd53522324ee8c74dcc4b099
Lisa Ciullo <lciullo@z3r2p6.42lyon.fr>
2024-04-16 13:30:58 +0200

    shitty code and changing variable name

--- backend/ft_transcendence/server/settings.py
+++ backend/ft_transcendence/server/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'test',
+        'HOST': 'test',
+        'PORT' : 5432
     }
 }
 
