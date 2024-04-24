bfaa0de89c74915894b7bc709ad7e6525d920e3e
Lisa Ciullo <lciullo@z3r3p6.42lyon.fr>
2024-04-24 16:48:20 +0200

    merge code

--- backend/ft_transcendence/run_server.sh
+++ backend/ft_transcendence/run_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
+python /backend/ft_transcendence/manage.py makemigrations
 python /backend/ft_transcendence/manage.py migrate
 python /backend/ft_transcendence/manage.py runserver 0.0.0.0:8000
