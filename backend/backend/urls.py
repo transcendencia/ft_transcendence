62f0fcae1883f4efe0141b8e7cf97d8a4123b004
clem <llovio.clemence@gmail.com>
2024-04-29 15:39:32 +0200

    It's compiling, and the structure is almost done

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ft_transcendence project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,9 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import authentication.views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', authentication.views.login_page, name='login'),
 ]
