1baed5bd02492c832f778545671f2331842af9b5
Dryff (Colas) <83179598+Dryff@users.noreply.github.com>
2024-05-09 00:23:37 +0200

    Removing players now work, i added a bot and a ball animation, i reformated the code to make it easier to understand

--- backend/static/html/js/arenaPage.js
+++ backend/static/html/js/arenaPage.js
@@ -1,20 +1,28 @@
 import { getTranslatedText } from "./loginPage.js";
 
-const thirdPlayerCont = document.querySelector('.thirdPlayerCont')
 const userlist = document.querySelector(".userlistBackground");
-const plusSigns = document.querySelectorAll(".plusPlayer");
+const userTiles = userlist.querySelectorAll(":scope > *");
+const plusButtons = document.querySelectorAll(".plusPlayer");
+
 const leftColumn = document.querySelector(".leftColumn");
 const userlistTitle = leftColumn.childNodes[1];
 
 let plusClicked = false;
 
+const blue = '#3777ff';
+const purple = 'rgb(164, 67, 255)'
+const grey = '#141414';
+const lightGrey = '#323232';
+
 function Glow() {
     userlist.style.transition = 'border-color 0.2s ease, box-shadow 0.2s ease';
     userlist.style.borderColor = '#ffb30e';
     userlist.style.animation = 'shadowBlink 1s infinite alternate ease-in-out';
-    userTiles.forEach(child => {
-        const children = child.querySelectorAll(":scope > *");
-        children.forEach(function(element) {
+    userTiles.forEach((tile, i) => {
+        if (profileAdded[i])
+            return;
+        const tileChildren = tile.querySelectorAll(":scope > *");
+        tileChildren.forEach(function(element) {
             element.style.transition = 'border-color 0.2s ease, box-shadow 0.2s ease';
             element.style.borderColor = '#ffb30e';
             element.style.animation = 'shadowBlink 1s infinite alternate ease-in-out';
@@ -23,96 +31,158 @@ function Glow() {
 }
 
 function resetGlow() {
-    userlist.style.borderColor = '#3777ff';
+    userlist.style.borderColor = blue;
     userlist.style.animation = '';
-    userTiles.forEach(child => {
+    userTiles.forEach((child, i) => {
         const children = child.querySelectorAll(":scope > *");
-        children.forEach(function(element) {
-            element.style.borderColor = '#3777ff';
+        children.forEach(element => {
+            element.style.borderColor = blue;
             element.style.animation = '';
         });
-    });
-    
-} 
-
-const userTiles = userlist.querySelectorAll(":scope > *");
-
-userlistTitle.textContent = getTranslatedText('userlistTitle');
-plusSigns.forEach(function(plusSign, i) {
-    plusSign.addEventListener('click', function () {
-        if (!plusClicked) {
-            Glow();
-            userlistTitle.textContent = getTranslatedText('userlistTitle2');
-            plusSign.style.backgroundColor = '#323232';
-            if (i === 0)
-                plusClicked = 1;
-            else plusClicked = 2;
-            plusSigns.forEach(function(otherPlusSign) {
-                if (otherPlusSign !== plusSign) {
-                    otherPlusSign.style.pointerEvents = 'none';
-                }
+        if (isBot(i)) {
+            children.forEach(element => {
+                element.style.borderColor = purple;
             });
         }
+    });
+} 
+
+function resetAddingMode() {
+    userlistTitle.textContent = getTranslatedText('userlistTitle');
+    plusClicked = 0;
+    plusButtons.forEach(function(otherPlusButton) {
+        otherPlusButton.style.pointerEvents = 'auto';
+    });
+    profileAdded[userTiles.length - 1] = false;
+}
+
+function setAddingMode(plusButton, i) {
+    userlistTitle.textContent = getTranslatedText('userlistTitle2');
+    if (i === 0) {
+        plusClicked = 1;
+        profileAdded[userTiles.length - 1] = true;
+    }
+    else plusClicked = 2;
+    plusButtons.forEach(function(otherPlusButton) {
+        if (otherPlusButton !== plusButton) {
+            otherPlusButton.style.pointerEvents = 'none';
+        }
+    });
+}
+
+userlistTitle.textContent = getTranslatedText('userlistTitle');
+plusButtons.forEach(function(plusButton, i) {
+    plusButton.addEventListener('click', function () {
+        if (!plusClicked) {
+            setAddingMode(plusButton, i);
+            Glow();
+            plusButton.style.backgroundColor = lightGrey;
+        }
         else {
+            resetAddingMode(plusButton);
             resetGlow();
-            userlistTitle.textContent = getTranslatedText('userlistTitle');
-            plusSign.style.backgroundColor = '#141414';
-            plusClicked = 0;
-            // Re-add click event listeners to all plus signs
-            plusSigns.forEach(function(otherPlusSign) {
-                otherPlusSign.style.pointerEvents = 'auto';
-            });
+            plusButton.style.backgroundColor = grey;
         }
     });
     //Hovering
-    plusSign.addEventListener('mouseenter', function () {
+    plusButton.addEventListener('mouseenter', function () {
         if (!plusClicked)
-            plusSign.style.backgroundColor = '#323232';
+            plusButton.style.backgroundColor = lightGrey;
     });
 
-    plusSign.addEventListener('mouseleave', function () {
+    plusButton.addEventListener('mouseleave', function () {
         if (!plusClicked)
-            plusSign.style.backgroundColor = '#141414';
+            plusButton.style.backgroundColor = grey;
     });
 });
 
 let profileAdded = [];
 
-//hovering for userTiles when a plusSign is clicked
-userTiles.forEach(tile => {
+function isBot(i) {
+    return (i === userTiles.length - 1)
+}
+
+function displayRemovePlayerVisual(userInfoCont, clonedImg, profilePic) {
+    clonedImg.src = 'assets/icons/whiteCross.png';
+    profilePic.style.borderColor = 'red';
+    userInfoCont.style.borderColor = 'red';
+    userInfoCont.style.fontSize = '25px';
+    userInfoCont.style.fontFamily = 'computer';
+    userInfoCont.childNodes[1].textContent = 'remove player';
+}
+
+function resetUserInfoVisual(userInfoCont, clonedImg, profilePic, tileText, i, tile) {
+    if (isBot(i)) {
+        userInfoCont.style.borderColor = purple;
+        profilePic.style.borderColor = purple;
+    }
+    else {
+        profilePic.style.borderColor = blue;
+        userInfoCont.style.borderColor = blue;
+    }
+    clonedImg.src = tile.querySelector('.imgContainer').querySelector('img').src;
+    userInfoCont.childNodes[1].textContent = tileText;
+    userInfoCont.style.fontFamily = 'space';
+    userInfoCont.style.fontSize = '15px';
+}
+
+function resetToPlusButton(userInfoCont, oldObj, textCont) {
+    userInfoCont.parentNode.replaceChild(oldObj, userInfoCont)
+    textCont.style.backgroundColor = '#00000031';
+    oldObj.style.backgroundColor = grey;
+}
+
+function createUserInfoObject(tile, i) {
+    const userInfoCont = document.createElement('div');
+    const profilePic = document.createElement('div');
+    const clonedImg = tile.querySelector('.imgContainer').querySelector('img').cloneNode(true);
+    const tileText = tile.querySelector('.textContainer').textContent;
+    const textNode = document.createTextNode(tileText);
+    userInfoCont.classList.add('userInfoCont');
+    profilePic.classList.add('profilePic');
+    profilePic.appendChild(clonedImg);
+    userInfoCont.appendChild(profilePic);
+    userInfoCont.appendChild(textNode);
+    if (isBot(i)) {
+        userInfoCont.style.borderColor = purple;
+        profilePic.style.borderColor = purple;
+    }
+    return {userInfoCont, clonedImg, profilePic, tileText};
+}
+
+userTiles.forEach((tile, i) => {
+    profileAdded[i] = false;
     tile.addEventListener('click', function(){
-        if (plusClicked) {
-            profileAdded[tile] = true;
-            const userInfoCont = document.createElement('div');
-            const profilePic = document.createElement('div');
-            const imgContainer = tile.querySelector('.imgContainer');
-            const img = imgContainer.querySelector('img');
-            const clonedImg = img.cloneNode(true);
-            const textContainer = tile.querySelector('.textContainer');
-            const tileText = textContainer.textContent;
-            const textNode = document.createTextNode(tileText);
-            const tileImgSrc = img.src;
-            clonedImg.src = tileImgSrc; // Update the cloned image source
-            userInfoCont.classList.add('userInfoCont');
-            profilePic.classList.add('profilePic');
-            profilePic.appendChild(clonedImg); // Append the cloned image
-            userInfoCont.appendChild(profilePic);
-            userInfoCont.appendChild(textNode);
-            const plusSign = plusSigns[plusClicked - 1];
-            plusSign.parentNode.replaceChild(userInfoCont, plusSign);
+        if (plusClicked && !profileAdded[i]) {
+            profileAdded[i] = true;
+            const newObj = createUserInfoObject(tile, i);
+            const oldObj = plusButtons[plusClicked - 1];
+            oldObj.parentNode.replaceChild(newObj.userInfoCont, oldObj);
             resetGlow();
-            plusClicked = 0;
+            resetAddingMode();
+            newObj.userInfoCont.addEventListener('mouseenter', function () {
+                displayRemovePlayerVisual(newObj.userInfoCont, newObj.clonedImg, newObj.profilePic);
+            });
+            newObj.userInfoCont.addEventListener('mouseleave', function () {
+                resetUserInfoVisual(newObj.userInfoCont, newObj.clonedImg, newObj.profilePic, newObj.tileText, i, tile);
+            });
+            newObj.userInfoCont.addEventListener('click', function() {
+                resetToPlusButton(newObj.userInfoCont, oldObj, textCont);
+                profileAdded[i] = false;
+                profileAdded[userTiles.length - 1] = false;
+            });
         }
     });
 
     const textCont = tile.querySelector(".textContainer");
     textCont.addEventListener('mouseenter', function () {
-        if (plusClicked && !profileAdded[tile])
+        if (plusClicked && !profileAdded[i])
             textCont.style.backgroundColor = 'rgba(90, 142, 255, 0.219)';
     });
     textCont.addEventListener('mouseleave', function () {
-        if (plusClicked && !profileAdded[tile])
+        if (plusClicked && !profileAdded[i])
             textCont.style.backgroundColor = '#00000031';
     });
 });
+
 
