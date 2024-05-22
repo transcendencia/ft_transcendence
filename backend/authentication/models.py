52171db4d9e85900c90ecc1cb99bfac3252440de
Clemence Llovio <cllovio@z3r2p6.42lyon.fr>
2024-05-22 17:22:25 +0200

    comment added to user class with all the fields that will be added soon

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -4,7 +4,14 @@ from django.shortcuts import render, redirect
 from django.db import IntegrityError
 
 class User(AbstractUser):
-	pass
+  pass
+  # languages
+  # game_won
+  # game_lost
+  # goals
+  # profile_picutre
+  # friends
+  # last_login_date
 
 class Member(models.Model):
   username = models.CharField(max_length=255)
