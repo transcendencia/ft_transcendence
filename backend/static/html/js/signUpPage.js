0ea1eea7dc3cb9b2c90a2248fd2d6167a5c52b4f
Maxime Finette <mfinette@z3r2p5.42lyon.fr>
2024-05-22 15:55:27 +0200

    production files removed and css bug temporarily removed ^^

--- backend/static/html/js/signUpPage.js
+++ backend/static/html/js/signUpPage.js
@@ -34,28 +34,28 @@ backToLoginButton.addEventListener('click', function() {
     moveCameraToBackOfCockpit();
 });
 
-// Add event listener to the sign-up form
-const signupForm = document.getElementById('signupForm');
-signupForm.addEventListener('submit', handleSignup);
+// // Add event listener to the sign-up form
+// const signupForm = document.getElementById('signupForm');
+// signupForm.addEventListener('submit', handleSignup);
 
-// Handle form submission
-function handleSignup(event) {
-    event.preventDefault();
+// // Handle form submission
+// function handleSignup(event) {
+//     event.preventDefault();
 
-    console.log(signupForm);
-    const formData = new FormData(event.target);
-    fetch('signup/', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => {
-        if (!response.ok) {
-            console.log(response)
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .catch(error => {
-        console.error('There was a problem with the sign-up:', error);
-    });
-}
+//     console.log(signupForm);
+//     const formData = new FormData(event.target);
+//     fetch('signup/', {
+//         method: 'POST',
+//         body: formData
+//     })
+//     .then(response => {
+//         if (!response.ok) {
+//             console.log(response)
+//             throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//     })
+//     .catch(error => {
+//         console.error('There was a problem with the sign-up:', error);
+//     });
+// }
