b139b8a75795de1f450373cab50acedae8ef5c11
Chapuis Paul <pchapuis@desktop-9ik33n8.home>
2024-05-29 15:31:19 +0200

    add save games result in tournament

--- backend/static/tournament/js/addGameData.js
+++ backend/static/tournament/js/addGameData.js
@@ -1,26 +1,31 @@
-async function createGame() {
-    const csrfToken = getCookie('csrftoken');  // You need to handle CSRF tokens if CSRF protection is enabled
-    const response = await fetch('/add_game/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken  // Include CSRF token in the request headers
-        },
-        body: JSON.stringify({
-            player1: 1,  // Replace with the actual player1 ID
-            player2: 2,  // Replace with the actual player2 ID
-            player3: 3,  // Replace with the actual player3 ID (optional)
-            scorePlayer1: 3,
-            scorePlayer2: 0,
-            gameplayMode: 'Classic'
-        })
-    });
+export async function createGame(player1Id, player2Id, player3Id, scorePlayer1, scorePlayer2, gameplayMode) {
+    const csrfToken = getCookie('csrftoken');
 
-    if (response.ok) {
-        const data = await response.json();
-        console.log('Game created successfully with ID:', data.game_id);
-    } else {
-        console.error('Failed to create game');
+    const payload = {
+        player1: player1Id,
+        player2: player2Id,
+        player3: player3Id,
+        scorePlayer1: scorePlayer1,
+        scorePlayer2: scorePlayer2,
+        gameplayMode: gameplayMode
+    };
+    try {
+        const response = await fetch('/add_game/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken  // Include CSRF token in the request headers
+            },
+            body: JSON.stringify(payload)
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Game created successfully with ID:', data.game_id);
+        } else {
+            console.error('Failed to create game');
+        }
+    } catch (error) {
+        console.error('Error creating game:', error);
     }
 }
 
@@ -39,9 +44,3 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-
-// Call the createGame function (for example, on form submission)
-document.querySelector('#createGameForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    createGame();
-});
