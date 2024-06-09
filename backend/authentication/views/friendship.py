a8c3fa4466faa4dc715a2fc90fe99d195a47dd68
clem <llovio.clemence@gmail.com>
2024-06-09 22:16:21 +0200

    language problem fixed, longin and signup message translated, Response changed to JSONRespon, uselless library deleted

--- backend/authentication/views/friendship.py
+++ backend/authentication/views/friendship.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render, redirect
-from django.views.decorators.csrf import (csrf_protect, csrf_exempt)
 from django.http import JsonResponse
-from django.utils import timezone
-from django.template.response import TemplateResponse
+from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
@@ -14,32 +10,15 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import ValidationError
 
 from ..models import User, FriendRequest
 from ..serializers import UserSerializer, SignupSerializer
 
-from django.shortcuts import redirect
-from django.contrib.auth import get_user_model
-from rest_framework.exceptions import ValidationError
-from django.db.models import Q
-
 def	send_friendrequest(request):
 	return render(request, 'send_friendrequest.html')
 
-# @api_view(['POST'])
-# @authentication_classes([TokenAuthentication])
-# @permission_classes([IsAuthenticated])
-# def	send_friend_request(request):
-# 	sender = request.user
-# 	receiver = User.objects.get(username=request.data.get('receiver_name'))
-# 	friend_request, created = FriendRequest.objects.get_or_create(sender=sender, receiver=receiver)
-# 	if created:
-# 		return Response("friend request sent")
-# 	else:
-# 		return Response("friend request not sent")
-
-
-User = get_user_model()
+User = get_user_model() #J'en ai vrmt besoin??
 
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
@@ -67,7 +46,7 @@ def send_friend_request(request):
     else:
         response = None
     
-    return Response({'message': response})
+    return JsonResponse({'message': response})
 #verifier qu'une friend request n'existe pas deja entre ces 2 user 
 
 @api_view(['POST'])
@@ -76,24 +55,20 @@ def send_friend_request(request):
 def	accept_friend_request(request):
 	friend_request = FriendRequest.objects.get(id=request.data.get("request_id"))
 	if friend_request.sender == request.user:
-		return Response('You cannot accept the invite if you are the sender')
-	print(friend_request.sender.username, friend_request.receiver.username)
+		return JsonResponse('You cannot accept the invite if you are the sender')
 	friend_request.status = "accepted"
 	friend_request.save()
-	return Response('friend request accepted')
-	# else:
-	# 	return Response('friend request not accepted')
+	return JsonResponse('friend request accepted')
+
 
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def reject_friend_request(request):
-    print("ca degage")
-    print(request.data.get("request_id"))
     friend_request = FriendRequest.objects.get(id=request.data.get("request_id"))
     friend_request.delete()
+
     return redirect('friend request rejected')
-    # return redirect(return_friends_list)
 #verifier que la requete que j'accepte c'est pas moi qui l'est envoyer
 
 def render_request(request):
@@ -103,20 +78,19 @@ def render_request(request):
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def return_request(request):
-    print(request.user.username)
     friend_requests = FriendRequest.objects.filter(Q(receiver=request.user) | Q(sender=request.user))
-    print(friend_requests)
     list_request = [{'id': req.id, 'status': req.status, 'sender': req.sender.username, 'receiver': req.receiver.username} for req in friend_requests]
-    print(list_request)
-    return Response({'list': list_request})
-    # return Response({"message": "je suis dans return message"})
+
+    return JsonResponse({'list': list_request})
 
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def return_friends_list(request):
     friends_requests = FriendRequest.objects.filter(Q(receiver=request.user) | Q(sender=request.user))
+
     pending_request_list = [{'id': req.id, 'status': req.status, 'sender': req.sender.username, 'receiver': req.receiver.username} for req in friends_requests if req.status == 'pending' and req.receiver == request.user]
     friends = [{'id': req.id, 'status': req.status, 'sender': req.sender.username, 'receiver': req.receiver.username} for req in friends_requests if req.status == 'accepted' and (req.receiver == request.user or req.sender == request.user)]
     waiting_request_list = [{'id': req.id, 'status': req.status, 'sender': req.sender.username, 'receiver': req.receiver.username} for req in friends_requests if req.status == 'pending' and req.sender == request.user]
-    return Response({'pending_request': pending_request_list, 'friends': friends, 'waiting_request': waiting_request_list})
+    
+    return JsonResponse({'pending_request': pending_request_list, 'friends': friends, 'waiting_request': waiting_request_list})
