c783971152189ba09e38a8445c4740090b98f513
Lisa Ciullo <lciullo@z3r3p5.42lyon.fr>
2024-06-11 13:19:29 +0200

    try to merge

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -11,10 +11,15 @@ then
     echo "PostgreSQL started"
 fi
 
+openssl genpkey -algorithm RSA -out privkey.pem -aes256
+openssl req -new -key privkey.pem -out cert.csr
+openssl x509 -req -days 365 -in cert.csr -signkey privkey.pem -out fullchain.pem
+
+
 # python manage.py makemigrations
 python manage.py makemigrations
 python manage.py makemigrations authentication
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
-# uvicorn backend.asgi:application --host 0.0.0.0 --port 8000
+#python manage.py runserver 0.0.0.0:8000 
+uvicorn backend.asgi:application --host 0.0.0.0 --port 8000  --port 443 --ssl-keyfile privkey.pem --ssl-certfile fullchain.pem
