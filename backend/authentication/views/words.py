b625429f7351bf36babe474f0d3a3aaa6ca1e24f
lciullo <lisa.ciullo42@gmail.com>
2024-06-14 18:10:18 +0200

    add two list of random words

---
+++ backend/authentication/views/words.py
@@ -0,0 +1,20 @@
+
+colors = [
+    'Aqua', 'Beige', 'Black', 'Blue', 'Brown', 'Coral', 'Crimson', 'Cyan', 'Gold', 'Gray',
+    'Green', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'Lime', 'Magenta', 'Maroon', 'Mauve', 'Navy',
+    'Olive', 'Pink', 'Plum', 'Purple', 'Red', 'Rose', 'Salmon', 'Silver', 'Teal', 'Turquoise',
+    'Violet', 'White', 'Yellow', 'Azure', 'Fuchsia', 'Chartreuse', 'Chocolate', 'Orchid', 'Sapphire',
+    'Tan', 'Periwinkle', 'Slate', 'Peach', 'Ruby', 'Amber', 'Emerald', 'Jade', 'Ruby', 'Saffron'
+]
+
+items = [
+    'apple', 'banana', 'cherry', 'grape', 'lemon', 'lime', 'pear', 'plum', 'peach', 'pineapple',
+    'blueberry', 'raspberry', 'strawberry', 'kiwi', 'watermelon', 'melon', 'coconut', 'mango', 'papaya', 'apricot',
+    'avocado', 'fig', 'guava', 'nectarine', 'blackberry', 'cranberry', 'date', 'dragonfruit', 'durian', 'lychee',
+    'passionfruit', 'plantain', 'quince', 'rambutan', 'starfruit', 'tamarind', 'breadfruit', 'clementine', 'grapefruit', 'kumquat',
+    'mandarin', 'persimmon', 'pomegranate', 'soursop', 'ugli', 'boysenberry', 'logenberry', 'asparagus', 'broccoli', 'cabbage', 
+    'cauliflower', 'celery', 'cucumber', 'eggplant', 'green-bean', 'lettuce', 'mushroom', 'onion', 'pea', 'potato', 'spinach', 
+    'zucchini', 'artichoke', 'brussels-sprout', 'beetroot', 'turnip', 'radish', 'bell-pepper', 'chili-pepper', 'okra', 'garlic', 
+    'leek', 'shallot', 'fennel', 'kale', 'swiss chard', 'chard', 'collard-greens', 'bok-choy', 'watercress', 'parsley', 'coriander', 
+    'cilantro', 'dill', 'basil', 'thyme', 'rosemary', 'oregano', 'lavender', 'chives', 'scallion', 'spring-onion', 'rhubarb'
+]
