a738827e740d6c54727485ca1e9d032975759729
lciullo <lisa.ciullo42@gmail.com>
2024-06-18 15:55:28 +0200

    I change pseudo hihiiiii now let's change profile picture by default

--- backend/authentication/views/words.py
+++ backend/authentication/views/words.py
@@ -1,20 +1,16 @@
 
 colors = [
-    'Aqua', 'Beige', 'Black', 'Blue', 'Brown', 'Coral', 'Crimson', 'Cyan', 'Gold', 'Gray',
-    'Green', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'Lime', 'Magenta', 'Maroon', 'Mauve', 'Navy',
-    'Olive', 'Pink', 'Plum', 'Purple', 'Red', 'Rose', 'Salmon', 'Silver', 'Teal', 'Turquoise',
-    'Violet', 'White', 'Yellow', 'Azure', 'Fuchsia', 'Chartreuse', 'Chocolate', 'Orchid', 'Sapphire',
-    'Tan', 'Periwinkle', 'Slate', 'Peach', 'Ruby', 'Amber', 'Emerald', 'Jade', 'Ruby', 'Saffron'
+    "Black", "White", "Red", "Green", "Blue", "Yellow", "Brown", "Orange", "Purple", "Pink",
+    "Beige", "Indigo", "Coral", "Peach", "Teal", "Olive", "Amber", "Azure", "Lilac", "Slate",
+    "Cream", "Magen", "Fusch", "Cyan", "Ivory", "Mauve", "Olive", "Peach", "Sienna", "Silver"
 ]
 
 items = [
-    'apple', 'banana', 'cherry', 'grape', 'lemon', 'lime', 'pear', 'plum', 'peach', 'pineapple',
-    'blueberry', 'raspberry', 'strawberry', 'kiwi', 'watermelon', 'melon', 'coconut', 'mango', 'papaya', 'apricot',
-    'avocado', 'fig', 'guava', 'nectarine', 'blackberry', 'cranberry', 'date', 'dragonfruit', 'durian', 'lychee',
-    'passionfruit', 'plantain', 'quince', 'rambutan', 'starfruit', 'tamarind', 'breadfruit', 'clementine', 'grapefruit', 'kumquat',
-    'mandarin', 'persimmon', 'pomegranate', 'soursop', 'ugli', 'boysenberry', 'logenberry', 'asparagus', 'broccoli', 'cabbage', 
-    'cauliflower', 'celery', 'cucumber', 'eggplant', 'green-bean', 'lettuce', 'mushroom', 'onion', 'pea', 'potato', 'spinach', 
-    'zucchini', 'artichoke', 'brussels-sprout', 'beetroot', 'turnip', 'radish', 'bell-pepper', 'chili-pepper', 'okra', 'garlic', 
-    'leek', 'shallot', 'fennel', 'kale', 'swiss chard', 'chard', 'collard-greens', 'bok-choy', 'watercress', 'parsley', 'coriander', 
-    'cilantro', 'dill', 'basil', 'thyme', 'rosemary', 'oregano', 'lavender', 'chives', 'scallion', 'spring-onion', 'rhubarb'
+    'Tulip', 'Lilac', 'Daisy', 'Violet', 'Rose', 'Orchid', 'Poppy', 'Lotus', 'Peony',
+    'Aster', 'Iris', 'Lily', 'Petal', 'Daffy', 'Gerda', 'Begon', 'Zinnia', 'Marig', 
+    'Edelw', 'Anem', 'Wister', 'Honeys', 'Sunfl', 'Hyaci', 'Azale', 'Jasmi', 'Dianth', 
+    'Dahli', 'Clover', 'Amary', 'Apple', 'Peach', 'Mango', 'Grape', 'Cherry', 'Banana', 
+    'Lemon', 'Melon', 'Kiwi', 'Lychee', 'Papaya', 'Orange', 'Guava', 'Fig', 'Dates', 'Plum', 
+    'Kiwif', 'Grape', 'Pomeg', 'Avoca'
 ]
+
