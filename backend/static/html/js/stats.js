fd9d02c4808f204fe9749eaecea9f08be6da56ee
mfinette <maxime@DESKTOP-KIMGHSS>
2024-06-19 18:58:17 +0200

    refactor of the stats class, new makefile rule for migrations

--- backend/static/html/js/stats.js
+++ backend/static/html/js/stats.js
@@ -32,10 +32,7 @@ class WinLostChart {
         ctx.font = options.fontSize + ' ' + options.fontFamily;
         ctx.fillStyle = options.fontColor;
         ctx.textAlign = 'center';
-        // ctx.textBaseline = 'middle';
         ctx.fillText(this.dataWinRate[0] + '%', width / 2, top + (height / 2));
-
-        // ctx.restore();
       }
     };
 
@@ -63,99 +60,95 @@ class WinLostChart {
       },
       plugins: [this.centerText]
     };
-  }
-  drawChart() {
-    if (this.chart) {
-      this.chart.destroy();
-    }
+
     this.chart = new Chart(
       document.getElementById(this.chartId),
       this.config
     );
   }
+
   updateData(newDataWinRate) {
     this.dataWinRate = newDataWinRate;
-    this.data.datasets[0].data = newDataWinRate;
-    this.drawChart();
+    this.chart.data.datasets[0].data = newDataWinRate;
+    this.chart.update();
   }
 }
+
 class DashesPowerChart {
-	constructor(chartId) {
-	  this.chartId = chartId;
-	  this.dataStats = [0, 0];
-	  this.chart = null;
-  
-	  this.data = {
-		labels: ['Dashes', 'Power Used'],
-		datasets: [{
-		  label: 'Percentage',
-		  data: this.dataStats,
-		  backgroundColor: [
-			"#f4ff69aa",
-			'#ff6e6eaa'
-		  ],
-		  borderColor: '#3777ff',
-		  cutout: '90%'
-		}]
-	  };
-  
-	  this.centerText = {
-		id: 'centerText',
-		beforeDraw: (chart, args, options) => {
-		  const { ctx, chartArea: { top, right, bottom, left, width, height } } = chart;
-		  ctx.save();
-  
-		  ctx.font = options.fontSize + ' ' + options.fontFamily;
-		  ctx.fillStyle = options.fontColor;
-		  ctx.textAlign = 'center';
-		  ctx.fillText(this.dataStats[0] + '%', width / 2, top + (height / 2));
-		}
-	  };
-  
-	  this.config = {
-		type: 'doughnut',
-		data: this.data,
-		options: {
-		  plugins: {
-			centerText: {
-			  fontColor: 'white',
-			  fontSize: '30px',
-			  fontFamily: 'Space'
-			},
-			title: {
-			  display: true,
-			  text: 'Dashes vs Power Used'
-			},
-			legend: {
-			  display: false,
-			  position: 'bottom'
-			}
-		  },
-		  responsive: true,
-		  maintainAspectRatio: false,
-		},
-		plugins: [this.centerText]
-	  };
-	}
-  
-	drawChart() {
-	  if (this.chart) {
-		this.chart.destroy();
-	  }
-	  this.chart = new Chart(
-		document.getElementById(this.chartId),
-		this.config
-	  );
-	}
-  
-	updateData(newDataStats) {
-	  this.dataStats = newDataStats;
-	  this.data.datasets[0].data = newDataStats;
-	  this.drawChart();
-	}
+  constructor(chartId) {
+    this.chartId = chartId;
+    this.dataStats = [0, 0];
+    this.chart = null;
+
+    this.data = {
+      labels: ['Dashes', 'Power Used'],
+      datasets: [{
+        label: 'Percentage',
+        data: this.dataStats,
+        backgroundColor: [
+          "#f4ff69aa",
+          '#ff6e6eaa'
+        ],
+        borderColor: '#3777ff',
+        cutout: '90%'
+      }]
+    };
+
+    this.centerText = {
+      id: 'centerText',
+      beforeDraw: (chart, args, options) => {
+        const { ctx, chartArea: { top, right, bottom, left, width, height } } = chart;
+        ctx.save();
+
+        ctx.font = options.fontSize + ' ' + options.fontFamily;
+        ctx.fillStyle = options.fontColor;
+        ctx.textAlign = 'center';
+        ctx.fillText(this.dataStats[0] + '%', width / 2, top + (height / 2));
+      }
+    };
+
+    this.config = {
+      type: 'doughnut',
+      data: this.data,
+      options: {
+        plugins: {
+          centerText: {
+            fontColor: 'white',
+            fontSize: '30px',
+            fontFamily: 'Space'
+          },
+          title: {
+            display: true,
+            text: 'Dashes vs Power Used'
+          },
+          legend: {
+            display: false,
+            position: 'bottom'
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+      plugins: [this.centerText]
+    };
+
+    this.chart = new Chart(
+      document.getElementById(this.chartId),
+      this.config
+    );
   }
-  
-  export function getUserStats(userId) {
+
+  updateData(newDataStats) {
+    this.dataStats = newDataStats;
+    this.chart.data.datasets[0].data = newDataStats;
+    this.chart.update();
+  }
+}
+
+const winLostChart = new WinLostChart('winLostStat');
+const dashesPowerChart = new DashesPowerChart('dashesPowerStat');
+
+export function getUserStats(userId) {
 	const token = localStorage.getItem('host_auth_token');
 	fetch(`get_stats/${userId}`, {
 	  method: 'GET',
@@ -172,14 +165,14 @@ class DashesPowerChart {
 	})
 	.then(data => {
 	  console.log(data);
-	  const winLostChart = new WinLostChart('winLostStat');
+	//   const winLostChart = new WinLostChart('winLostStat');
 	  winLostChart.updateData([data.percentageGameWon, data.percentageGameLost]);
 	  
-	  const dashesPowerChart = new DashesPowerChart('dashesPowerStat');
-	dashesPowerChart.updateData([
+	//   const dashesPowerChart = new DashesPowerChart('dashesPowerStat');
+	  dashesPowerChart.updateData([
 		parseFloat(data.dashesPercentage.toFixed(2)),
 		parseFloat(data.poweredUsedPercentage.toFixed(2))
-	]);
+	  ]);
 	  
 	  console.log("currentStreak", data.currentStreak);
 	})
