e55538985358c7d69af8184cb561c95b5ca05032
lciullo <lisa.ciullo42@gmail.com>
2024-06-24 19:03:32 +0200

    add server in production mode

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -11,7 +11,6 @@ then
     echo "PostgreSQL started"
 fi
 
-# python manage.py makemigrations
 python manage.py makemigrations
 python manage.py makemigrations authentication
 python manage.py migrate
@@ -37,10 +36,8 @@ if not User.objects.filter(username=USER_USERNAME).exists():
     user.save()
 EOF
 
-mkdir /cert
+#mkdir /cert
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /cert/key.pem -out /cert/cert.pem -subj "/CN=localhost"
+#uvicorn backend.asgi:application --host 0.0.0.0 --port 8000 --ssl-keyfile /cert/key.pem --ssl-certfile /cert/cert.pem
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /cert/key.pem -out /cert/cert.pem -subj "/CN=localhost"
-
-#python manage.py runserver 0.0.0.0:8000
-
-uvicorn backend.asgi:application --host 0.0.0.0 --port 8000 --ssl-keyfile /cert/key.pem --ssl-certfile /cert/cert.pem
+python manage.py runserver 0.0.0.0:8000
