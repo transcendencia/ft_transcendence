e8fac302452d039c36f6ddf4091114a2bef5ef0a
lciullo <lisa.ciullo42@gmail.com>
2024-06-29 12:31:28 +0200

    try to install crontab

--- backend/cmd/delete_inactive_users.py
+++ backend/cmd/delete_inactive_users.py
@@ -1,8 +1,13 @@
-from ... models import User
+from .. models import User
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from datetime import timedelta
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def deleteInactiveUser():
     print("On rentre dans la fonction crontab")
+    logger.debug("test")
