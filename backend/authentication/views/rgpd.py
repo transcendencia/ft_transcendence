721ad7211cfd731c96899de162e56c24029d123f
lciullo <lisa.ciullo42@gmail.com>
2024-07-02 11:04:44 +0200

    generate a file.txt with data of user

--- backend/authentication/views/rgpd.py
+++ backend/authentication/views/rgpd.py
@@ -75,8 +75,9 @@ def generateDataFile(request):
         filename = f"user_data_{slugify(user.username)}_{timestamp}.txt"
         response = HttpResponse(buffer.getvalue(), content_type='text/plain')
         response['Content-Disposition'] = f'attachment; filename="{filename}"'
-
+        print(response['Content-Disposition'])
         logger.info(f"File generated successfully for user {user.username}")
+        print(response)
         return response
     except Exception as e:
         logger.error(f"Error generating file for user {request.user.username}: {str(e)}")
