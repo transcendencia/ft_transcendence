2d837f88400d736fa1d0d831eb872c7ad752cb4f
Maxime Finette <mfinette@z3r3p3.42lyon.fr>
2024-07-11 10:44:26 +0200

    stats transition changed, and game endscreen now has custom message if in game or in tournament.

--- backend/static/html/js/stats.js
+++ backend/static/html/js/stats.js
@@ -135,18 +135,12 @@ export function chooseStats(stat) {
 	const stat5 = document.getElementById('statsBlock5');
 	const stat6 = document.getElementById('statsBlock6');
 
-	if (stat > 3)
-	{
-		removeClasses(stat4);
-		removeClasses(stat5);
-		removeClasses(stat6);
-	}
-	else
-	{
-		removeClasses(stat1);
-		removeClasses(stat2);
-		removeClasses(stat3);
-	}
+	removeClasses(stat4);
+	removeClasses(stat5);
+	removeClasses(stat6);
+	removeClasses(stat1);
+	removeClasses(stat2);
+	removeClasses(stat3);
 
     if (stat === 1) {
         if (stat2.style.display === 'flex') {
