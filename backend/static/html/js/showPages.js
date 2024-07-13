280e7d5064a810c6d2e24a5a18e5c7c7359481a1
mfinette <maxime.finette1@gmail.com>
2024-07-13 02:26:13 +0200

    browser history fixed when you reload the page

--- backend/static/html/js/showPages.js
+++ backend/static/html/js/showPages.js
@@ -1,17 +1,17 @@
-import { moveCameraToBackOfCockpit, moveCameraToFrontOfCockpit} from "./signUpPage.js";
+import { moveCameraToBackOfCockpit, moveCameraToFrontOfCockpit } from "./signUpPage.js";
 
 export function showPage(pageId, transition = 'default') {
     var pages = document.querySelectorAll('.page');
     pages.forEach(function(page) {
         page.classList.remove('show'); // Remove the 'show' class from all pages
-        //remove transitions
+        // Remove transitions
         page.classList.remove('default');
         page.classList.remove('signUp');
     });
     // Show the selected page
-    if (pageId == 'none')
-        return;
+    if (pageId === 'none') return;
     window.location.hash = `#${pageId}`;
+    sessionStorage.setItem('currentPage', pageId); // Store current page in sessionStorage
     pageId = '.' + pageId;
     var selectedPage = document.querySelector(pageId);
     selectedPage.classList.add('show'); // Add the 'show' class to the selected page
@@ -20,27 +20,22 @@ export function showPage(pageId, transition = 'default') {
 
 let oldLocation = window.location.hash || '#loginPage';
 
-if (!window.location.hash) {
-    window.location.hash = '#loginPage';
-    oldLocation = '#loginPage';
-    showPage('loginPage');
-} else {
-    showPage(window.location.hash.substring(1)); // Show the current page based on the hash
-}
-
+// Retrieve the last page from sessionStorage if it exists
+const lastPage = sessionStorage.getItem('currentPage') || 'loginPage';
+window.location.hash = `#${lastPage}`;
+showPage(lastPage);
 
 addEventListener("hashchange", () => {
-    if (window.location.hash == '#loginPage' && oldLocation == '#signUpPage' )
-        moveCameraToBackOfCockpit();   
-    else if (window.location.hash == '#signUpPage' && oldLocation == '#loginPage')
+    if (window.location.hash === '#loginPage' && oldLocation === '#signUpPage') {
+        moveCameraToBackOfCockpit();
+    } else if (window.location.hash === '#signUpPage' && oldLocation === '#loginPage') {
         moveCameraToFrontOfCockpit();
-    else if (window.location.hash == '#rgpdPage' && oldLocation == '#signUpPage'){
+    } else if (window.location.hash === '#rgpdPage' && oldLocation === '#signUpPage') {
         showPage('rgpdPage');
         const RGPDPage = document.querySelector(".rgpdPage");
         RGPDPage.classList.add("perspectived");
-    }
-    else if (window.location.hash == '#signUpPage' && oldLocation == '#rgpdPage') {
-        showPage('signUpPage')
+    } else if (window.location.hash === '#signUpPage' && oldLocation === '#rgpdPage') {
+        showPage('signUpPage');
     }
     oldLocation = window.location.hash;
 });
